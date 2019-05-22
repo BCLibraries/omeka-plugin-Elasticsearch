2e865233790464540731494aaecc6fcb0ea37e57
Benjamin Florin <benjamin.florin@gmail.com>
2019-05-22 11:01:09 -0400

    Add Query class

---
+++ libraries/Elasticsearch/Model/Query.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/SubQuery.php';
+require_once __DIR__ . '/AggregationList.php';
+
+class Elasticsearch_Model_Query
+{
+    private $subqueries = [];
+    private $filters = [];
+    private $aggregations;
+
+    public function __construct(array $subqueries, array $filters, Elasticsearch_Model_AggregationList $aggregations)
+    {
+        $this->subqueries = $subqueries;
+        $this->filters = $filters;
+        $this->aggregations = $aggregations;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'query' => [
+                'bool' => [
+                    'must' => array_map([$this, 'subQueryToArray'], $this->subqueries),
+                    'filter' => array_map([$this, 'subQueryToArray'], $this->filters)
+                ]
+            ],
+            'aggregations' => $this->aggregations->toArray()
+        ];
+    }
+
+    private function subQueryToArray(Elasticsearch_Model_SubQuery $subquery): array
+    {
+        return $subquery->toArray();
+    }
+}
