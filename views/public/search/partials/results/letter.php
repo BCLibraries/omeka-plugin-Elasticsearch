bac4a9dc406753340ec5333d590d8fb882c3c949
Benjamin Florin <benjamin.florin@gmail.com>
2019-06-06 15:41:15 -0400

    Don't show images in search results

--- views/public/search/partials/results/letter.php
+++ views/public/search/partials/results/letter.php
@@ -1,8 +1,4 @@
 <?php
-try {
-    $result_img = record_image($record, 'thumbnail', ['class' => 'elasticsearch-result-image']);
-} catch (Exception $e) {
-}
 
 $elements = [];
 if (isset($hit['elements'])) {
@@ -20,8 +16,6 @@ $recipients_heading = count($elements['audience']) > 1 ? 'Recipients' : 'Recipie
 $destinations_heading = count($elements['publisher']) > 1 ? 'Destinations' : 'Destination';
 
 ?>
-<?php if ($result_img): ?><a href="<?= $url ?>"><?= $result_img ?></a><?php endif; ?>
-
 <ul>
     <?php if ($recipients): ?>
         <li title="recipient"><b><?= $recipients_heading ?>:</b> <?= $recipients ?></li>
