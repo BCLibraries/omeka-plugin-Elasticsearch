2424680f02ed84662bd93b3061b4e35f9624fdee
Benjamin Florin <benjamin.florin@gmail.com>
2019-06-07 13:15:10 -0400

    Hide archive aggregation for now

--- views/public/search/partials/aggregations.php
+++ views/public/search/partials/aggregations.php
@@ -2,6 +2,12 @@
 $aggregation_labels = Elasticsearch_Helper_Aggregations::getAggregationLabels();
 $applied_facets = Elasticsearch_Helper_Aggregations::getAppliedFacets();
 $result_facets = Elasticsearch_Helper_Aggregations::getResultFacets($aggregations);
+
+// @todo enable archive facet when there are multiple archives
+$result_facets = array_filter($result_facets, function (Elasticsearch_Model_Facet $facet) {
+    return $facet->name !== 'archive';
+});
+
 $url_without_facets = get_view()->url('/elasticsearch') . '?q=' . urlencode($query['q']);
 ?>
 
@@ -23,13 +29,13 @@ $url_without_facets = get_view()->url('/elasticsearch') . '?q=' . urlencode($que
 <div id="elasticsearch-filters">
     <h3>Filters</h3>
     <?php foreach ($result_facets as $facet): ?>
-            <h4><?= $facet->label ?></h4>
-            <ul>
-                <?php foreach ($facet->buckets as $bucket): ?>
-                    <li>
-                        <a href="<?= $bucket->url() ?>"><?= $bucket->value ?></a> (<?= $bucket->count ?>)
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+        <h4><?= $facet->label ?></h4>
+        <ul>
+            <?php foreach ($facet->buckets as $bucket): ?>
+                <li>
+                    <a href="<?= $bucket->url() ?>"><?= $bucket->display_value ?></a> (<?= $bucket->count ?>)
+                </li>
+            <?php endforeach; ?>
+        </ul>
     <?php endforeach; ?>
 </div>
